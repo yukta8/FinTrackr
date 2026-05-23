7d80827dffda77c76b7cb3719f78bb1e1549f3b4
Yukta Agrawal <67012335+yukta8@users.noreply.github.com>
2026-05-23 10:04:03 +0530

    Update routes.py

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,1 +1,19 @@
+from fastapi import APIRouter,UploadFile, File , Depends, HTTPException
+from sqlmodel import Session
+import pandas as pd
+from app.db import get_session
+from app.models import Transaction
+from datetime import datetime
+
+router=APIRouter()
+
+@router.post("/upload-csv")
+async def upload_csv(file: UploadFile = File(...), session: Session=Depends(get_session)):
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(status_code=400, detail="File extension not supported, Please upload a csv file!")
+
+    contents = await file.read()
+    df = pd.read_csv(pd.io.common.BytesIO(contents))
+
+    
 
